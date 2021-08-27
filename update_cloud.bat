a590bd665d3c1c8d3c6f4a4fadf7c1e0d998bdd0
Francisco Javier Sánchez Barrado <fjsanchez@atsistemas.com>
2021-08-27 11:18:37 +0200

    cambios cloud

--- update_cloud.bat
+++ update_cloud.bat
@@ -2,3 +2,5 @@ SET PATH=C:\GCloudSDK\google-cloud-sdk\bin;C:\openjdk_1.8.0\bin;%PATH%;
 gcloud config set project dilosohairapp
 mvn clean package -P prod
 gcloud app deploy .\target\dilosohairapp\WEB-INF\appengine-web.xml --version=r8-0-9 --no-stop-previous-version --no-promote
+gcloud app deploy .\target\dilosohairapp\WEB-INF\cron.yaml
+
