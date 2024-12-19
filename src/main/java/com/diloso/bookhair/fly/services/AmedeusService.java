05cbfabe0c736d043cccb039c19f278aca42d5c7
Francisco Javier Sánchez Barrado <fjsanchez@knowmadmood.com>
2024-12-19 09:59:01 +0100

    ignoreAm

--- src/main/java/com/diloso/bookhair/fly/services/AmedeusService.java
+++ src/main/java/com/diloso/bookhair/fly/services/AmedeusService.java
@@ -10,8 +10,8 @@ import jakarta.inject.Singleton;
 @Singleton
 public class AmedeusService {
 
-	private static String CLIENT_ID = "aa";
-	private static String CLIENT_SECRET = "aa";
+	private static String CLIENT_ID = "bb";
+	private static String CLIENT_SECRET = "bb";
 
 	protected static final Amadeus amadeusInstance = Amadeus.builder(CLIENT_ID, CLIENT_SECRET).build();
 
